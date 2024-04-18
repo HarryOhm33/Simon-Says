9ed748d7c9ada52ce072fe10a9358acde7abce72
Hari Om <hari333333om@gmail.com>
2024-04-18 22:00:00 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,127 @@
+let gameSeq = [];
+let userSeq = [];
+
+let started = false;
+let level = 0;
+let highScore = 0;
+
+let btns = ["red", "green", "yellow", "blue"];
+
+let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
+let allBtns = document.querySelectorAll(".btn");
+let startBtn = document.querySelector(".start-btn");
+let endBtn = document.querySelector(".end-btn");
+
+let h4 = document.querySelector("h4");
+let body = document.querySelector("body");
+
+startBtn.addEventListener("click", () => {
+  if (started == false) {
+    started = true;
+    startBtn.classList.add("d-none");
+    endBtn.classList.remove("d-none");
+    levelUp();
+  }
+});
+
+function levelUp() {
+  userSeq = [];
+  level++;
+  h3.innerText = `Level ${level}`;
+
+  let randIdx = Math.floor(Math.random() * 4);
+  let randColor = btns[randIdx];
+  let randBtn = document.querySelector(`.${randColor}`);
+
+  gameSeq.push(randColor);
+
+  if (level == 1) {
+    setTimeout(() => {
+      gameFlash(randBtn);
+    }, 500);
+  } else {
+    gameFlash(randBtn);
+  }
+}
+
+function gameFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(() => {
+    btn.classList.remove("flash");
+  }, 200);
+}
+
+for (let btn of allBtns) {
+  btn.addEventListener("click", userRes);
+}
+
+function userRes() {
+  if (started == true) {
+    let btn = this;
+
+    let userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    userFlash(btn);
+
+    checkAns(userSeq.length - 1);
+  }
+}
+
+function userFlash(btn) {
+  btn.classList.add("userFlash");
+  setTimeout(() => {
+    btn.classList.remove("userFlash");
+  }, 200);
+}
+
+function checkAns(Idx) {
+  if (userSeq[Idx] == gameSeq[Idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h3.innerHTML = `Game Over! Your Score was :- ${
+      level - 1
+    } </br> Press The Button To Start Again`;
+
+    startBtn.classList.remove("d-none");
+
+    if (highScore < level) {
+      highScore = level - 1;
+      h4.innerText = `HighScore = ${highScore}`;
+    }
+
+    bodyFlash();
+    reset();
+  }
+}
+
+function reset() {
+  started = false;
+  level = 0;
+  userSeq = [];
+  gameSeq = [];
+  endBtn.classList.add("d-none");
+}
+
+function bodyFlash() {
+  let prev = body.getAttribute("style");
+  body.setAttribute("style", "background: #f2003c");
+  setTimeout(() => {
+    body.setAttribute("style", `background: ${prev}`);
+  }, 200);
+}
+
+endBtn.addEventListener("click", () => {
+  h3.innerHTML = `Press The Button To Start Again`;
+
+  startBtn.classList.remove("d-none");
+
+  if (highScore < level) {
+    highScore = level - 1;
+    h4.innerText = `HighScore = ${highScore}`;
+  }
+
+  reset();
+});
